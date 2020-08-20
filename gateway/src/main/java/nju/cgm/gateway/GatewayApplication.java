eef6e92b788636cce647c21339e8895d5e1ae61f
Bright Chan <33258775+CGM397@users.noreply.github.com>
2020-08-20 22:19:45 +0800

    add Spring Cloud Config and zuul.

---
+++ gateway/src/main/java/nju/cgm/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package nju.cgm.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
